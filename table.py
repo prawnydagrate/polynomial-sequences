d22383c80bc962d6a030b328f1a8bb8c3e2974b5
Pranav <pranav.pooruli@gmail.com>
2024-10-26 10:24:10 +0530

    finally figured out that i had to use css grid

--- table.py
+++ table.py
@@ -17,24 +17,23 @@ def make_table(degree: int) -> list[list[Term]]:
     return table
 
 def to_html(table: list[list[Term]]) -> str:
-    css = "<style>td, th { border: 1px solid black; text-align: center; font-family: monospace; padding: 4px; } table { border-collapse: collapse; }</style>"
-    html = "<!DOCTYPE html><html><head></head><body><table>"
     ncols = math.lcm(*(len(row) for row in table))
+    css = ".table { width: fit-content; display: grid; grid-template-columns: min-content repeat(%s, 1fr); gap: 1px; background-color: black; border: 1px solid black; } " % ncols \
+        + '.td, .th { padding: 4px; text-align: center; background-color: white; font-family: "Courier New", Courier, monospace; } ' \
+        + ".th { font-weight: bold } "
+    html = '<!DOCTYPE html><html><head></head><body><div class="table">'
     for (i, row) in enumerate(table):
         rowlen = len(row)
         colspan = ncols//rowlen
+        style = f"grid-column: span {colspan}"
         if i == 0:
-            html += "<tr>"
-            html += "<th>n</th>"
+            html += '<div class="th td">n</div>'
             for n in range(rowlen):
-                html += f'<td colspan="{colspan}">{n+1}</td>'
-            html += "</tr>"
-        html += "<tr>"
-        html += f"<th>{"u" if i == 0 else f"d<sub>{i}</sub>"}</th>"
+                html += f'<div class="td" style="{style}">{n+1}</div>'
+        html += f'<div class="th td">{"u" if i == 0 else f"d<sub>{i}</sub>"}</div>'
         for term in row:
-            html += f'<td colspan="{colspan}">'
+            html += f'<div class="td" style="{style}">'
             html += " + ".join(f"{comp.coefficient if comp.coefficient != 1 else ""}a<sub>{comp.var}</sub>" for comp in term.components if comp.coefficient != 0)
-            html += "</td>"
-        html += "</tr>"
-    html += f"</table>{css}</body></html>"
+            html += "</div>"
+    html += f"</div><style>{css}</style></body></html>"
     return html
